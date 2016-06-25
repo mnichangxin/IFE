b7cf4330de10d962985c5fc0085985da9b6106d8
mnichangxin <mnichangxin@vip.qq.com>
2016-06-25 22:08:12 +0800

    modify

---
+++ second_stage/task33/index.js
@@ -0,0 +1,39 @@
+var flag = 'top'; //记录方块方向
+
+window.onload = function() {
+	var btn = document.getElementById('button');
+
+	btn.addEventListener('click', handler, false);
+}
+
+function handler() {
+	var content = document.getElementById('content').value.trim(),
+	                     block = document.getElementById('block');
+
+	var top = block.style.top,
+	left = block.style.right;
+	console.log(block.style.position);
+	switch(content) {
+		case 'GO':
+			if (flag == 'top' && top != 0) {
+				block.style.top = top - 50 + 'px';
+			}
+			if (flag == 'bottom' && top != 450) {
+				block.style.top = top + 50 + 'px';
+			}
+			if (flag == 'left' && left != 0) {
+				block.style.left = left - 50 + 'px';
+			}
+			if (flag == 'right' && left != 450) {
+				block.style.left = left + 50 + 'px';
+			}
+			break;
+		case 'TUN LEF':
+			break;
+		case 'TUN RIG':
+			break;
+		case 'BAC':
+			break;
+	}
+}
+
